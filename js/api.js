54f19439efb21d0b2bb8fdf0074a988b47d9e226
The King Lion <kinglionkl250@gmail.com>
2024-05-12 20:32:50 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,54 +1,81 @@
-const API_URL = "productos.json";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
 
-async function obtenerProductos() {
-  try {
-    const response = await fetch(API_URL);
-    if (!response.ok) {
-      throw new Error("No se pudo obtener la lista de productos.");
+const firebaseConfig = {
+    apiKey: "AIzaSyDjF7Jt-3V-4Ja_sAovbBHkgekmVW84AaQ",
+    authDomain: "alurageek-3fad8.firebaseapp.com",
+    databaseURL: "https://alurageek-3fad8-default-rtdb.firebaseio.com",
+    projectId: "alurageek-3fad8",
+    storageBucket: "alurageek-3fad8.appspot.com",
+    messagingSenderId: "145565268054",
+    appId: "1:145565268054:web:294c729c470ea066ade3f7",
+    measurementId: "G-50KML50LCL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+async function listarProductos(API_URL) {
+    try {
+        const response = await fetch(API_URL);
+
+        if(!response.ok) {
+            throw new Error("No se pudo obtener la lista de productos.");
+        }
+
+        const data = await response.json();
+        return data;
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al obtener los productos:", error);
-    throw error; // Propaga el error para que pueda ser manejado externamente
-  }
+    catch(error) {
+        console.error("Error al obtener los productos:", error);
+        throw error;
+    }
 }
 
-async function crearProducto(nuevoProducto) {
-  try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(nuevoProducto)
-    });
-    if (!response.ok) {
-      throw new Error("No se pudo crear el nuevo producto.");
+async function crearProducto(nombre, precio, imagen, url) {
+    try {
+        const response = await fetch(url,  {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                precio: precio,
+                imagen: imagen
+            })
+        });
+
+        if(!response.ok) {
+            throw new Error("No se pudo crear el nuevo producto.");
+        }
+
+        const data = await response.json();
+        return data;
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al crear el nuevo producto:", error);
-    throw error; // Propaga el error para que pueda ser manejado externamente
-  }
+    catch(error) {
+        console.error("Error al crear el nuevo producto:", error);
+        throw error;
+    }
 }
 
-async function eliminarProducto(id) {
-  const url = `${API_URL}/${id}`;
-  try {
-    const response = await fetch(url, {
-      method: "DELETE"
-    });
-    if (!response.ok) {
-      throw new Error("No se pudo eliminar el producto.");
+async function eliminarProducto(id, url) {
+    try {
+        const response = await fetch(`${url}${id}`, {
+            method: "DELETE"
+        });
+
+        if(!response.ok) {
+            throw new Error("No se pudo eliminar el producto.");
+        }
+
+        const data = await response.json();
+        return data;
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al eliminar el producto:", error);
-    throw error; // Propaga el error para que pueda ser manejado externamente
-  }
+    catch(error) {
+        console.error("Error al eliminar el producto:", error);
+        throw error;
+    }
 }
 
-export { obtenerProductos, crearProducto, eliminarProducto };
+export const API = { listarProductos, crearProducto, eliminarProducto };
